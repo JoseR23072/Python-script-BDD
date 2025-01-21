d41679ea8c05b2910adfea81e3d5ae7ea467f39b
DESKTOP-47KV5R7\jose <josequispeogosi12927@gmail.com>
2025-01-21 12:57:59 +0100

    commit - url peces arreglado

--- sql_peces/Anaecypris hispanica.sql
+++ sql_peces/Anaecypris hispanica.sql
@@ -1,1 +1,1 @@
-UPDATE peces peces SET imagen_url='https://www.joelsartore.com/wp-content/uploads/stock/FIS039/FIS039-00037-1920x1280.jpg' WHERE nombre_cientifico='Anaecypris hispanica';
+UPDATE peces peces SET imagen_url='https://www.plantasyhongos.es/herbarium/a/Anaecypris_hispanica_03.jpg' WHERE nombre_cientifico='Anaecypris hispanica';
