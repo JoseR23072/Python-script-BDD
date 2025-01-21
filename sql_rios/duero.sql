4adf30d94eb76df5c044725c03ca674d9b7c7f9f
DESKTOP-47KV5R7\jose <josequispeogosi12927@gmail.com>
2025-01-21 12:42:13 +0100

    commit - sql de rios añadido

---
+++ sql_rios/duero.sql
@@ -0,0 +1,7 @@
+INSERT INTO rios (nombre,descripcion) VALUES (
+    "Río Duero",
+    "El río Duero es el cuerpo de agua dulce con mayor caudal de la península ibérica, incluyendo España y Portugal. Además, es el tercer río más largo de España y el más importante del noroeste del país. Discurre por las provincias españolas de Burgos, Salamanca, Soria, Valladolid y Zamora; y los distritos portugueses de Aveiro, Braganza, Guarda, Oporto, Vila Real y Viseo.
+
+Posee una longitud de 897 kilómetros, un caudal medio de 800 m³/s y una cuenca hidrográfica que ocupa cerca de 98.073 km². Su nacimiento se ubica a 2.160 msnm en los Picos de Urbión del municipio español Duruelo de la Sierra, en la provincia de Soria; y su desembocadura se encuentra en el océano Atlántico, entre los municipios portugueses de Oporto y Vila Nova de Gaia."
+)
+
---
+++ sql_rios/guadiana.sql
@@ -0,0 +1,6 @@
+INSERT INTO rios (nombre,descripcion) VALUES (
+    "Río Guadiana",
+    "El río Guadiana es un cuerpo de agua dulce del sudoeste de la península ibérica que discurre por las comunidades autónomas españolas de Andalucía, Castilla-La Mancha y Extremadura y las regiones tradicionales portuguesas de Alentejo y Algarve.
+
+Posee una longitud de aproximadamente 818 kilómetros, una cuenca hidrográfica que ocupa 67.733 km² y un caudal medio que varía entre 26 m³/s y 78 m³/s. En un tramo mayor a 100 kilómetros, el río actúa como frontera natural entre España y Portugal. Cerca del 80% de su cuenca pertenece a territorio español y el 20% restante a territorio portugués."
+)
